42170057e8b28c2226ab80801c5b7139ade4fa15
MichaelDeets <michael@deets.sh>
2023-10-07 02:09:37 +0100

    added sh

---
+++ post.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+vibrant-cli DP-2 1
+#bspc config focus_follows_pointer true
+
+export ENABLE_VKBASALT="0"
+export ENABLE_VK_LAYER_TORKEL104_libstrangle="0"
+export MANGOHUD="0"
---
+++ pre.sh
@@ -0,0 +1,13 @@
+export ENABLE_VKBASALT="1"
+export VKBASALT_LOG_LEVEL="none"
+export ENABLE_VK_LAYER_TORKEL104_libstrangle="1"
+export STRANGLE_AF="1"
+export STRANGLE_BICUBIC="1"
+export STRANGLE_PICMIP="1"
+export STRANGLE_RETRO="1"
+export STRANGLE_TRILINEAR="1"
+export MANGOHUD="0"
+export MANGOHUD_CONFIG="engine_version,frametime,vulkan_driver"
+export AMD_TEX_ANISO="0"
+export RADV_DEBUG=novrsflatshading,nodynamicbounds
+export RADV_TEX_ANISO="0"
